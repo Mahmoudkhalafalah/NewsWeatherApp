152b19892a6a99fddcee9b88d10508d3fc2597dc
omar tarek <ot482002@gmail.com>
2024-04-26 02:02:12 +0300

    add widget

--- lib/views/weather_search_view.dart
+++ lib/views/weather_search_view.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
 
-class search_view extends StatelessWidget {
-  const search_view({Key? key}) : super(key: key);
+class Search_View_Page extends StatelessWidget {
+  const Search_View_Page ({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        //backgroundColor: Colors.indigo,
-        title: const Text (
-          'Search for a city',
-          style: TextStyle(color: Colors.white),
+        title: const Text(
+            'Search a city' ,
+         style: TextStyle(
+           color: Colors.white,
+         ),
         ),
       ),
-      body: ,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Enter a city',
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: (){},
+              child: const Text('Search'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
