f4581ed98d2e0dba157d14ecf6513d515a1e8e69
omar tarek <ot482002@gmail.com>
2024-04-26 01:32:15 +0300

    add weather_search_view.dart

---
+++ lib/views/weather_search_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class search_view extends StatelessWidget {
+  const search_view({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //backgroundColor: Colors.indigo,
+        title: const Text(
+          'Search for a city',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: ,
+    );
+  }
+}
