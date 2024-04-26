530f4a3e5687cec22f6f9b5a0fc0d57fb5546a57
omar tarek <ot482002@gmail.com>
2024-04-26 03:32:57 +0300

    add cubit structure

---
+++ lib/cubits/get_weather_cubit/get_weather_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../services/weather_serivce.dart';
+import 'package:news_weather_app_project/cubits/get_weather_cubit/get_weather_states.dart';
+
+class GetWeatherCubit extends Cubit<WeatherState> {
+  GetWeatherCubit() : super(InitialState());
+
+  getweather ({ required String cityname }) async {
+    try {
+      /*
+      * weathermode
+      * */
+
+      emit();
+    }
+    catch(e){
+      emit();
+    }
+  }
+}
